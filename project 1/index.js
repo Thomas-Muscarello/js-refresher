fb2da8713d6890ad60d036393405f549f8e55ef9
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-07-26 18:50:37 -0400

    intro

---
+++ project 1/index.js
@@ -0,0 +1,5 @@
+// First... there are two ways to comment within your code via JS
+    // One is my using the double "//"
+    // The other, for multi line comments, you can use /* ---insert text here--- */
+
+    
