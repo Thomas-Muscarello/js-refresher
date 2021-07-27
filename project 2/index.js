9a5ba9d9104db469a94f98aa3cae86a57c750a46
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-07-26 20:58:30 -0400

    declarations

---
+++ project 2/index.js
@@ -0,0 +1,14 @@
+// Unintialized Values : Means that values are undefined
+let a;
+let b; 
+let c; 
+
+//To initialize, set a value
+let a = 5;
+let b = 10;
+let c = "I am a string"
+
+//Variable decalration is case sensative
+
+let tom !== let Tom
+
