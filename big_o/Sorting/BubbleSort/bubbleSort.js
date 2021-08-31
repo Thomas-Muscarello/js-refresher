f354350f8d64b35911d0361e108ef72166d5be53
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-08-30 20:27:22 -0400

    bubbleSort with break

--- big_o/Sorting/BubbleSort/bubbleSort.js
+++ big_o/Sorting/BubbleSort/bubbleSort.js
@@ -4,15 +4,25 @@
 //return sorted array
 
 function bubbleSort(arr){
+    let noSwaps;
+    //Use i in the definition of J so that i can move down and that you dont toucj the last number after each loop
     for(let i = arr.length; i>0; i--){
+        //Set swap to true
+        noSwaps=true
+        //See if j is greater then the last number of I
         for(let j = 0; j<i-1; j++){
+            //if you need to swap
            if(arr[j]>arr[j+1]){
                //SWAP
             let temp = arr[j];
             arr[j]=arr[j+1];
             arr[j+1]=temp;
+            //if there is a swap, change it to false
+            noSwaps=false
             }
         }
+        //If no swaps stays true
+        if(noSwaps) break;
     }
     return arr
 }
