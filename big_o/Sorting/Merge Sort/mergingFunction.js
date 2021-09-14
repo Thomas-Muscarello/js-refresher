c1acda2670c0b8b8613736ee9cec4cbd9d408c82
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-09-13 21:03:56 -0400

    Merging

---
+++ big_o/Sorting/Merge Sort/mergingFunction.js
@@ -0,0 +1,33 @@
+//Assume Arrays are sorted
+//Merge two sorted arrays will all elements sorted
+//Should run in O(n+m) time and O(n+m) space should not modify parameters passed in
+//Two Counters start at zero
+//use while loops
+//Start with comparing the frist two elements, add the smaller number to a new array... one at a time
+
+function merge(arr1,arr2){
+    let results = []
+    let i = 0
+    let j = 0
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j]>arr1[i]){
+            results.push(arr1[i]);
+            i++
+        } else {
+            results.push(arr2[j])
+            j++
+        }
+    }
+    //Add remaining items if the arrays aren't the same length
+    while(i<arr1.length){
+        results.push(arr1[i])
+        i++
+    }
+    while(j<arr2.length){
+        results.push(arr1[j])
+        j++
+    }
+    return results;
+}
+
+merge([1,10,50],[2,14,99,100])
